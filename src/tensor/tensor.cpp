dfc59ee961c8f99afbd9fe7c39cb0f2d9ca16489
blegouix <stilynx51@gmail.com>
2024-09-21 19:01:15 +0000

    setup src/ folder

---
+++ src/tensor/tensor.cpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+#include <ddc/ddc.hpp>
+
+#include "tensor.hpp"
+
+Tensor::Tensor() {
+    printf("Tensor created");
+}
---
+++ src/tensor/tensor.hpp
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+#pragma once
+
+#include <ddc/ddc.hpp>
+
+class Tensor {
+public:
+    explicit Tensor();
+};
