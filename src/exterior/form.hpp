379e52bb1567145952eb7b118a34c2ae32a9b782
blegouix <stilynx51@gmail.com>
2024-11-15 12:50:50 +0000

    missing file

---
+++ src/exterior/form.hpp
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: 2024 Baptiste Legouix
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <ddc/ddc.hpp>
+
+#include "cochain.hpp"
+#include "cosimplex.hpp"
+
+namespace sil {
+
+namespace exterior {
+
+template <class T, class ElementType, class Allocator>
+struct FormWrapper;
+
+template <misc::NotSpecialization<Chain> T, class ElementType, class Allocator>
+struct FormWrapper<T, ElementType, Allocator>
+{
+    using type = Cosimplex<T, ElementType>;
+};
+
+template <misc::Specialization<Chain> T, class ElementType, class Allocator>
+struct FormWrapper<T, ElementType, Allocator>
+{
+    using type = Cochain<T, ElementType, Allocator>;
+};
+
+/*
+template <misc::NotSpecialization<Chain> Head, class ElementType>
+FormWrapper(Head, ElementType) -> FormWrapper<Head, ElementType, std::allocator<ElementType>>;
+
+template <misc::Specialization<Chain> Head, class ElementType, class Allocator>
+FormWrapper(Head, ElementType, Allocator) -> FormWrapper<Head, ElementType, Allocator>;
+*/
+
+// Usage should be avoided because CTAD cannot go through it
+template <class T, class ElementType = double, class Allocator = std::allocator<double>>
+using Form = typename FormWrapper<T, ElementType, Allocator>::type;
+
+} // namespace exterior
+
+} // namespace sil
