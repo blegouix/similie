ec7aa00ca0fc2366aba4bad6694d44957c7aa9f7
blegouix <stilynx51@gmail.com>
2024-11-28 19:39:40 +0000

    rc

--- tests/exterior/hodge_star.cpp
+++ tests/exterior/hodge_star.cpp
@@ -47,16 +47,21 @@ struct Rho : sil::tensor::TensorNaturalIndex<X, Y, Z>
 {
 };
 
-using MuLow = sil::tensor::TensorContravariantNaturalIndex<Mu>;
-using NuLow = sil::tensor::TensorContravariantNaturalIndex<Nu>;
-using RhoUp = sil::tensor::TensorCovariantNaturalIndex<Rho>;
+using MuUp = sil::tensor::TensorContravariantNaturalIndex<Mu>;
+using MuLow = sil::tensor::TensorCovariantNaturalIndex<Mu>;
+using NuUp = sil::tensor::TensorContravariantNaturalIndex<Nu>;
+using NuLow = sil::tensor::TensorCovariantNaturalIndex<Nu>;
+using RhoUp = sil::tensor::TensorContravariantNaturalIndex<Rho>;
+using RhoLow = sil::tensor::TensorCovariantNaturalIndex<Rho>;
 
 using MetricIndex = sil::tensor::TensorSymmetricIndex<
         sil::tensor::TensorCovariantNaturalIndex<sil::tensor::MetricIndex1<X, Y, Z>>,
         sil::tensor::TensorCovariantNaturalIndex<sil::tensor::MetricIndex2<X, Y, Z>>>;
 
 using HodgeStarDomain = sil::exterior::
-        hodge_star_domain_t<ddc::detail::TypeSeq<MuLow, NuLow>, ddc::detail::TypeSeq<RhoUp>>;
+        hodge_star_domain_t<ddc::detail::TypeSeq<MuUp, NuUp>, ddc::detail::TypeSeq<RhoLow>>;
+using HodgeStarDomain2 = sil::exterior::
+        hodge_star_domain_t<ddc::detail::TypeSeq<RhoUp>, ddc::detail::TypeSeq<MuLow, NuLow>>;
 
 TEST(HodgeStar, Test)
 {
@@ -96,12 +101,61 @@ TEST(HodgeStar, Test)
 
     sil::exterior::fill_hodge_star<
             MetricIndex,
-            ddc::detail::TypeSeq<Mu, Nu>,
-            ddc::detail::TypeSeq<Rho>>(hodge_star, metric);
-    std::cout << hodge_star.natural_domain().front() << std::endl;
-    std::cout << hodge_star.natural_domain().back() << std::endl;
-    ddc::for_each(hodge_star.mem_domain(), [&] (auto elem) {
-        std::cout << hodge_star.mem(elem) << std::endl;
+            ddc::detail::TypeSeq<MuUp, NuUp>,
+            ddc::detail::TypeSeq<RhoLow>>(hodge_star, metric);
+
+    sil::tensor::TensorAccessor<sil::tensor::TensorAntisymmetricIndex<MuLow, NuLow>> form_accessor;
+    ddc::DiscreteDomain<DDimX, DDimY, sil::tensor::TensorAntisymmetricIndex<MuLow, NuLow>>
+            form_dom(metric.non_indices_domain(), form_accessor.mem_domain());
+    ddc::Chunk form_alloc(form_dom, ddc::HostAllocator<double>());
+    sil::tensor::Tensor<
+            double,
+            ddc::DiscreteDomain<DDimX, DDimY, sil::tensor::TensorAntisymmetricIndex<MuLow, NuLow>>,
+            Kokkos::layout_right,
+            Kokkos::DefaultHostExecutionSpace::memory_space>
+            form(form_alloc);
+    ddc::for_each(mesh_xy, [&](ddc::DiscreteElement<DDimX, DDimY> elem) {
+        form(elem, form.accessor().access_element<X, Y>()) = 1.;
+        form(elem, form.accessor().access_element<X, Z>()) = 2.;
+        form(elem, form.accessor().access_element<Y, Z>()) = 3.;
     });
-    std::cout << hodge_star[mesh_xy.front()];
+
+    sil::tensor::TensorAccessor<RhoLow> dual_form_accessor;
+    ddc::DiscreteDomain<DDimX, DDimY, RhoLow>
+            dual_form_dom(metric.non_indices_domain(), dual_form_accessor.mem_domain());
+    ddc::Chunk dual_form_alloc(dual_form_dom, ddc::HostAllocator<double>());
+    sil::tensor::Tensor<
+            double,
+            ddc::DiscreteDomain<DDimX, DDimY, RhoLow>,
+            Kokkos::layout_right,
+            Kokkos::DefaultHostExecutionSpace::memory_space>
+            dual_form(dual_form_alloc);
+
+    ddc::for_each(mesh_xy, [&](ddc::DiscreteElement<DDimX, DDimY> elem) {
+        sil::tensor::tensor_prod(dual_form[elem], hodge_star[elem], form[elem]);
+    });
+
+    sil::tensor::tensor_accessor_for_domain_t<HodgeStarDomain2> hodge_star_accessor2;
+    ddc::cartesian_prod_t<ddc::DiscreteDomain<DDimX, DDimY>, HodgeStarDomain2>
+            hodge_star_dom2(metric.non_indices_domain(), hodge_star_accessor2.mem_domain());
+    ddc::Chunk hodge_star_alloc2(hodge_star_dom2, ddc::HostAllocator<double>());
+    sil::tensor::Tensor<
+            double,
+            ddc::cartesian_prod_t<ddc::DiscreteDomain<DDimX, DDimY>, HodgeStarDomain2>,
+            Kokkos::layout_right,
+            Kokkos::DefaultHostExecutionSpace::memory_space>
+            hodge_star2(hodge_star_alloc2);
+
+    sil::exterior::fill_hodge_star<
+            MetricIndex,
+            ddc::detail::TypeSeq<RhoUp>,
+            ddc::detail::TypeSeq<MuLow, NuLow>>(hodge_star2, metric);
+
+    ddc::parallel_fill(form, 0.);
+    ddc::for_each(mesh_xy, [&](ddc::DiscreteElement<DDimX, DDimY> elem) {
+        sil::tensor::tensor_prod(form[elem], hodge_star2[elem], dual_form[elem]);
+        EXPECT_DOUBLE_EQ(form(elem, form.accessor().access_element<X, Y>()), 1.);
+        EXPECT_DOUBLE_EQ(form(elem, form.accessor().access_element<X, Z>()), 2.);
+        EXPECT_DOUBLE_EQ(form(elem, form.accessor().access_element<Y, Z>()), 3.);
+    });
 }
