2f245e7132c78798286aba5690f52003a3c3ceaf
Baptiste Legouix <stilynx51@gmail.com>
2024-12-02 13:13:18 +0000

    wip

--- toolchains/v100.spack/prepare.sh
+++ toolchains/v100.spack/prepare.sh
@@ -1,9 +1,12 @@
+# SPDX-FileCopyrightText: 2024 Baptiste Legouix
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 export SPACK_USER_PREFIX="${WORKDIR}/spack"
 
 git clone -c feature.manyFiles=true --depth=2 https://github.com/spack/spack.git ${SPACK_USER_PREFIX}
 . ${SPACK_USER_PREFIX}/share/spack/setup-env.sh
 
 spack external find cmake 
-spack external find cuda
-spack install ginkgo@1.8.0%gcc@11 cuda_arch=70 ^cuda@11.5+allow-unsupported-compilers
+spack install cuda
+spack install ginkgo@1.8.0 cuda_arch=70 +cuda
 spack install fftw -mpi +openmp precision=double 
