3fe568967a1305a94f0ea107b509810c19081e98
Baptiste Legouix <stilynx51@gmail.com>
2024-12-02 17:39:47 +0000

    rc

--- toolchains/v100.spack/environment.sh
+++ toolchains/v100.spack/environment.sh
@@ -4,3 +4,8 @@
 export SPACK_USER_PREFIX="${WORKDIR}/spack"
 
 . ${SPACK_USER_PREFIX}/share/spack/setup-env.sh
+
+spack load cuda
+spack load openblas
+spack load fftw
+spack load ginkgo
--- toolchains/v100.spack/prepare.sh
+++ toolchains/v100.spack/prepare.sh
@@ -6,7 +6,9 @@ export SPACK_USER_PREFIX="${WORKDIR}/spack"
 git clone -c feature.manyFiles=true --depth=2 https://github.com/spack/spack.git ${SPACK_USER_PREFIX}
 . ${SPACK_USER_PREFIX}/share/spack/setup-env.sh
 
-spack external find cmake 
+spack external find cmake
+spack compiler find
 spack install cuda
+spack install openblas
+spack install fftw -mpi +openmp
 spack install ginkgo@1.8.0 cuda_arch=70 +cuda
-spack install fftw -mpi +openmp precision=double 
