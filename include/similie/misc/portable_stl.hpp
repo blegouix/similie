8fa5f0d7e7991bfbbcc07cda5eb0201f9d446b6a
blegouix <stilynx51@gmail.com>
2024-12-11 12:31:56 +0000

    wip

---
+++ include/similie/misc/portable_stl.hpp
@@ -0,0 +1,96 @@
+// SPDX-FileCopyrightText: 2024 Baptiste Legouix
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <Kokkos_Core.hpp>
+
+namespace sil {
+
+namespace misc {
+
+namespace detail {
+
+template <class InputIt, class T = typename std::iterator_traits<InputIt>::value_type>
+KOKKOS_FUNCTION constexpr InputIt find(InputIt first, InputIt last, const T& value)
+{
+    for (; first != last; ++first)
+        if (*first == value)
+            return first;
+
+    return last;
+}
+
+/*
+template <class InputIt, class UnaryPred>
+KOKKOS_FUNCTION constexpr InputIt find_if(InputIt first, InputIt last, UnaryPred p)
+{
+    for (; first != last; ++first)
+        if (p(*first))
+            return first;
+
+    return last;
+}
+
+template <class InputIt, class UnaryPred>
+KOKKOS_FUNCTION constexpr InputIt find_if_not(InputIt first, InputIt last, UnaryPred q)
+{
+    for (; first != last; ++first)
+        if (!q(*first))
+            return first;
+
+    return last;
+}
+
+template <class InputIt, class UnaryPred>
+KOKKOS_FUNCTION constexpr bool all_of(InputIt first, InputIt last, UnaryPred p)
+{
+    return find_if_not(first, last, p) == last;
+}
+*/
+
+template <class T>
+KOKKOS_FUNCTION constexpr std::remove_reference_t<T>&& move(T&& t) noexcept
+{
+    return static_cast<typename std::remove_reference<T>::type&&>(t);
+}
+
+template <class InputIt, class OutputIt>
+KOKKOS_FUNCTION OutputIt move(InputIt first, InputIt last, OutputIt d_first)
+{
+    for (; first != last; ++d_first, ++first)
+        *d_first = move(*first);
+
+    return d_first;
+}
+
+template <class I>
+KOKKOS_FUNCTION constexpr std::size_t bounded_advance(I& i, std::size_t n, I const bound)
+{
+    for (; n > 0 && i != bound; --n, void(++i)) {
+        ;
+    }
+
+    return n;
+}
+
+template <class ForwardIt>
+KOKKOS_FUNCTION ForwardIt shift_left(ForwardIt first, ForwardIt last, std::size_t n)
+{
+    if (n <= 0) {
+        return last;
+    }
+
+    auto mid = first;
+    if (bounded_advance(mid, n, last)) {
+        return first;
+    }
+
+    return move(move(mid), move(last), move(first));
+}
+
+} // namespace detail
+
+} // namespace misc
+
+} // namespace sil
