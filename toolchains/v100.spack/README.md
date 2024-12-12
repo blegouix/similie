b7dfb2f1461fe7709bbaaf3f3a5baa8a576e2043
Baptiste Legouix <stilynx51@gmail.com>
2024-12-12 13:04:26 +0000

    init

---
+++ toolchains/v100.spack/README.md
@@ -0,0 +1,4 @@
+Tested on OVH T1-LE-45 instance with Ubuntu 24
+- Run ubuntu24.sh to install spack dependencies and nvidia driver. It will reboot.
+- Run prepare.sh to install spack environment
+- Run environment.sh to load spack environment
---
+++ toolchains/v100.spack/ubuntu24.sh
@@ -0,0 +1,10 @@
+sudo apt install -y build-essential
+sudo apt install -y cmake
+sudo apt install -y bzip2 ca-certificates file gcc-13 g++-13 gfortran-13 git gzip lsb-release patch python3 tar unzip xz-utils zstd
+if ! nvidia-smi &> /dev/null; then
+    wget https://uk.download.nvidia.com/tesla/560.35.03/nvidia-driver-local-repo-ubuntu2404-560.35.03_1.0-1_amd64.deb
+    sudo cp /var/nvidia-driver-local-repo-ubuntu2404-560.35.03/nvidia-driver-local-BCE469C4-keyring.gpg /usr/share/keyrings/
+    sudo dpkg -i nvidia-driver-local-repo-ubuntu2404-560.35.03_1.0-1_amd64.deb
+    sudo apt update
+    sudo apt install -y nvidia-driver-560
+fi
