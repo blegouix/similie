592214be9e4621237e7a9f8b661a8c7eb51f5c6a
Baptiste Legouix <stilynx51@gmail.com>
2024-12-17 13:38:50 +0100

    Export HDF5 2D Laplacian example result (#63)
    - Setup for PDI
    - Export HDF5 data from 2D_Laplacian
    - Export XDMF file, tested in Paraview

--- examples/2d_laplacian.cpp
+++ examples/2d_laplacian.cpp
@@ -3,8 +3,81 @@
 
 #include <ddc/ddc.hpp>
 #include <ddc/kernels/splines.hpp>
+#include <ddc/pdi.hpp>
 
 #include <similie/similie.hpp>
+
+// PDI config
+constexpr char const* const PDI_CFG = R"PDI_CFG(
+metadata:
+  Nx : int
+  Ny : int
+
+data:
+  X:
+    type: array
+    subtype: double
+    size: [ '$Nx', '$Ny' ]
+  Y:
+    type: array
+    subtype: double
+    size: [ '$Nx', '$Ny' ]
+  potential:
+    type: array
+    subtype: double
+    size: [ '$Nx', '$Ny' ]
+  laplacian:
+    type: array
+    subtype: double
+    size: [ '$Nx-1', '$Ny-1' ]
+
+plugins:
+  decl_hdf5:
+    - file: '2d_laplacian.h5'
+      on_event: [export]
+      collision_policy: replace_and_warn
+      write: [Nx, Ny, X, Y, potential, laplacian]
+  #trace: ~
+)PDI_CFG";
+
+// XDMF
+int write_xdmf(int Nx, int Ny)
+{
+    constexpr char const* const xdmf = R"XDMF(<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+ <Domain>
+   <Grid Name="mesh1" GridType="Uniform">
+     <Topology TopologyType="2DSMesh" NumberOfElements="%i %i"/>
+     <Geometry GeometryType="X_Y">
+       <DataItem Dimensions="%i %i" NumberType="Float" Precision="8" Format="HDF">
+        2d_laplacian.h5:/X
+       </DataItem>
+       <DataItem Dimensions="%i %i" NumberType="Float" Precision="8" Format="HDF">
+        2d_laplacian.h5:/Y
+       </DataItem>
+     </Geometry>
+     <Attribute Name="Potential" AttributeType="Scalar" Center="Node">
+       <DataItem Dimensions="%i %i" NumberType="Float" Precision="8" Format="HDF">
+        2d_laplacian.h5:/potential
+       </DataItem>
+     </Attribute>
+     <Attribute Name="Laplacian" AttributeType="Scalar" Center="Cell">
+       <DataItem Dimensions="%i %i" NumberType="Float" Precision="8" Format="HDF">
+        2d_laplacian.h5:laplacian/
+       </DataItem>
+     </Attribute>
+   </Grid>
+ </Domain>
+</Xdmf>
+)XDMF";
+
+    FILE* file = fopen("2d_laplacian.xmf", "w");
+    fprintf(file, xdmf, Nx, Ny, Nx, Ny, Nx, Ny, Nx, Ny, Nx - 1, Ny - 1);
+    fclose(file);
+
+    return 1;
+}
 
 static constexpr std::size_t s_degree = 3;
 
@@ -72,18 +145,37 @@ using DummyIndex = sil::tensor::TensorCovariantNaturalIndex<sil::tensor::TensorN
 
 int main(int argc, char** argv)
 {
+    PC_tree_t conf_pdi = PC_parse_string(PDI_CFG);
+    PC_errhandler(PC_NULL_HANDLER);
+    PDI_init(conf_pdi);
+
     Kokkos::ScopeGuard const kokkos_scope(argc, argv);
     ddc::ScopeGuard const ddc_scope(argc, argv);
-
-    printf("start example\n");
 
     MesherXY mesher;
     ddc::Coordinate<X, Y> lower_bounds(-5., -5.);
     ddc::Coordinate<X, Y> upper_bounds(5., 5.);
-    ddc::DiscreteVector<DDimX, DDimY> nb_cells(40, 40);
+    ddc::DiscreteVector<DDimX, DDimY> nb_cells(1000, 1000);
     ddc::DiscreteDomain<DDimX, DDimY> mesh_xy = mesher.template mesh<
             ddc::detail::TypeSeq<DDimX, DDimY>,
             ddc::detail::TypeSeq<BSplinesX, BSplinesY>>(lower_bounds, upper_bounds, nb_cells);
+    ddc::expose_to_pdi("Nx", static_cast<int>(mesh_xy.template extent<DDimX>()));
+    ddc::expose_to_pdi("Ny", static_cast<int>(mesh_xy.template extent<DDimY>()));
+
+    // Allocate and instantiate a position field (used only to be exported).
+    [[maybe_unused]] sil::tensor::TensorAccessor<MuUp> position_accessor;
+    ddc::DiscreteDomain<MuUp, DDimX, DDimY> position_dom(mesh_xy, position_accessor.mem_domain());
+    ddc::Chunk position_alloc(position_dom, ddc::HostAllocator<double>());
+    sil::tensor::Tensor position(position_alloc);
+    ddc::parallel_for_each(
+            Kokkos::DefaultHostExecutionSpace(),
+            mesh_xy,
+            KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX, DDimY> elem) {
+                position(elem, position.accessor().access_element<X>())
+                        = static_cast<double>(ddc::coordinate(ddc::DiscreteElement<DDimX>(elem)));
+                position(elem, position.accessor().access_element<Y>())
+                        = static_cast<double>(ddc::coordinate(ddc::DiscreteElement<DDimY>(elem)));
+            });
 
     // Allocate and instantiate a metric tensor field.
     [[maybe_unused]] sil::tensor::TensorAccessor<MetricIndex> metric_accessor;
@@ -113,12 +205,13 @@ int main(int argc, char** argv)
     Kokkos::deep_copy(
             inv_metric.allocation_kokkos_view(),
             metric.allocation_kokkos_view()); // FIXME: Temporary patch
-
+    /*
     auto debug = inv_metric;
     auto debug_host = ddc::create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), debug);
     std::cout << "DEBUG:" << std::endl;
     std::cout << sil::tensor::Tensor(debug_host[debug.accessor().mem_domain().front()])
               << std::endl;
+    */
 
     // Potential
     [[maybe_unused]] sil::tensor::TensorAccessor<DummyIndex> potential_accessor;
@@ -130,6 +223,7 @@ int main(int argc, char** argv)
     ddc::parallel_for_each(
             potential.domain(),
             KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX, DDimY, DummyIndex> elem) {
+                double const R = 2.;
                 double const r = Kokkos::sqrt(
                         static_cast<double>(
                                 ddc::coordinate(ddc::DiscreteElement<DDimX>(elem))
@@ -137,18 +231,14 @@ int main(int argc, char** argv)
                         + static_cast<double>(
                                 ddc::coordinate(ddc::DiscreteElement<DDimY>(elem))
                                 * ddc::coordinate(ddc::DiscreteElement<DDimY>(elem))));
-                if (r <= 1) {
-                    potential.mem(elem) = -Kokkos::numbers::pi_v<double> / 2 * r * r;
+                if (r <= R) {
+                    potential.mem(elem) = 6.25 * r * r;
                 } else {
-                    potential.mem(elem) = -Kokkos::numbers::pi_v<double> * Kokkos::log(r);
+                    potential.mem(elem) = 6.25 * (Kokkos::log(r / R) + (R * R));
                 }
             });
-
-    std::cout << "Potential:" << std::endl;
     auto potential_host
             = ddc::create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), potential);
-    std::cout << sil::tensor::Tensor(potential_host[potential_accessor.mem_domain().front()])
-              << std::endl;
 
     // Gradient
     [[maybe_unused]] sil::tensor::TensorAccessor<MuLow> gradient_accessor;
@@ -156,6 +246,7 @@ int main(int argc, char** argv)
     ddc::Chunk gradient_alloc(gradient_dom, ddc::DeviceAllocator<double>());
     sil::tensor::Tensor gradient(gradient_alloc);
     sil::exterior::deriv<MuLow, DummyIndex>(Kokkos::DefaultExecutionSpace(), gradient, potential);
+    Kokkos::fence();
 
     // Hodge star
     [[maybe_unused]] sil::tensor::tensor_accessor_for_domain_t<HodgeStarDomain> hodge_star_accessor;
@@ -168,6 +259,7 @@ int main(int argc, char** argv)
             sil::tensor::upper<MetricIndex>,
             ddc::detail::TypeSeq<MuUp>,
             ddc::detail::TypeSeq<NuLow>>(Kokkos::DefaultExecutionSpace(), hodge_star, inv_metric);
+    Kokkos::fence();
 
     // Dual gradient
     [[maybe_unused]] sil::tensor::TensorAccessor<NuLow> dual_gradient_accessor;
@@ -182,6 +274,7 @@ int main(int argc, char** argv)
             KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX, DDimY> elem) {
                 sil::tensor::tensor_prod(dual_gradient[elem], gradient[elem], hodge_star[elem]);
             });
+    Kokkos::fence();
 
     // Dual Laplacian
     [[maybe_unused]] sil::tensor::TensorAccessor<
@@ -194,6 +287,7 @@ int main(int argc, char** argv)
     sil::tensor::Tensor dual_laplacian(dual_laplacian_alloc);
     sil::exterior::
             deriv<RhoLow, NuLow>(Kokkos::DefaultExecutionSpace(), dual_laplacian, dual_gradient);
+    Kokkos::fence();
 
     // Hodge star 2
     [[maybe_unused]] sil::tensor::tensor_accessor_for_domain_t<HodgeStarDomain2>
@@ -207,6 +301,7 @@ int main(int argc, char** argv)
             sil::tensor::upper<MetricIndex>,
             ddc::detail::TypeSeq<RhoUp, NuUp>,
             ddc::detail::TypeSeq<>>(Kokkos::DefaultExecutionSpace(), hodge_star2, inv_metric);
+    Kokkos::fence();
 
     // Laplacian
     [[maybe_unused]] sil::tensor::TensorAccessor<DummyIndex> laplacian_accessor;
@@ -222,10 +317,23 @@ int main(int argc, char** argv)
             KOKKOS_LAMBDA(ddc::DiscreteElement<DDimX, DDimY> elem) {
                 sil::tensor::tensor_prod(laplacian[elem], dual_laplacian[elem], hodge_star2[elem]);
             });
+    Kokkos::fence();
 
     auto laplacian_host
             = ddc::create_mirror_view_and_copy(Kokkos::DefaultHostExecutionSpace(), laplacian);
-    std::cout << "Laplacian:" << std::endl;
-    std::cout << sil::tensor::Tensor(laplacian_host[laplacian_accessor.mem_domain().front()])
-              << std::endl;
+
+    ddc::PdiEvent("export")
+            .with("X", position[position.accessor().access_element<X>()])
+            .and_with("Y", position[position.accessor().access_element<Y>()])
+            .and_with("potential", potential_host)
+            .and_with("laplacian", laplacian_host);
+    std::cout << "Computation result exported in 2d_laplacian.h5" << std::endl;
+    PC_tree_destroy(&conf_pdi);
+    PDI_finalize();
+
+    write_xdmf(
+            static_cast<int>(mesh_xy.template extent<DDimX>()),
+            static_cast<int>(mesh_xy.template extent<DDimY>()));
+
+    return EXIT_SUCCESS;
 }
--- toolchains/v100.spack/prepare.sh
+++ toolchains/v100.spack/prepare.sh
@@ -3,16 +3,25 @@
 
 export SPACK_USER_PREFIX="${WORKDIR}/spack"
 
+# Spack
 git clone -c feature.manyFiles=true --depth=2 https://github.com/spack/spack.git ${SPACK_USER_PREFIX}
 . ${SPACK_USER_PREFIX}/share/spack/setup-env.sh
 
+# PDI repo
+git clone https://github.com/pdidev/spack.git ${SPACK_USER_PREFIX}/var/spack/repos/pdi
+spack repo add ${SPACK_USER_PREFIX}/var/spack/repos/pdi
+
+# Install environment
 spack env create similie
 spacktivate similie
 spack external find
 spack external find cuda
 spack compiler find
+spack add cmake
 spack add cuda
 spack add netlib-lapack
 spack add fftw -mpi +openmp
 spack add ginkgo@1.8.0 cuda_arch=70 +cuda
+spack add pdi -fortran -python
+spack add pdiplugin-decl-hdf5 -fortran -mpi
 spack install
