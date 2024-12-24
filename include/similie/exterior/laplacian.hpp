626ce96a4c064d4b57e5dc2f282c58f44013e839
Baptiste Legouix <stilynx51@gmail.com>
2024-12-24 03:10:47 +0000

    typo

--- include/similie/exterior/laplacian.hpp
+++ include/similie/exterior/laplacian.hpp
@@ -19,7 +19,7 @@ namespace exterior {
 
 template <
         tensor::TensorIndex MetricIndex,
-        tensor::TensorNatIndex LaplacianDummyTag,
+        tensor::TensorNatIndex LaplacianDummyIndex,
         tensor::TensorIndex CochainTag,
         misc::Specialization<tensor::Tensor> TensorType,
         misc::Specialization<tensor::Tensor> MetricType,
@@ -31,29 +31,29 @@ TensorType laplacian(
         MetricType inv_metric)
 {
     static_assert(CochainTag::rank() == 0); // TODO support higher forms
-    static_assert(tensor::is_covariant_v<LaplacianDummyTag>);
+    static_assert(tensor::is_covariant_v<LaplacianDummyIndex>);
 
     // Derivative
-    [[maybe_unused]] tensor::TensorAccessor<coboundary_index_t<LaplacianDummyTag, CochainTag>>
+    [[maybe_unused]] tensor::TensorAccessor<coboundary_index_t<LaplacianDummyIndex, CochainTag>>
             derivative_accessor;
     ddc::cartesian_prod_t<
             typename TensorType::non_indices_domain_t,
-            ddc::DiscreteDomain<coboundary_index_t<LaplacianDummyTag, CochainTag>>>
+            ddc::DiscreteDomain<coboundary_index_t<LaplacianDummyIndex, CochainTag>>>
             derivative_tensor_dom(tensor.non_indices_domain(), derivative_accessor.mem_domain());
     ddc::Chunk derivative_tensor_alloc(
             derivative_tensor_dom,
             ddc::KokkosAllocator<double, typename ExecSpace::memory_space>());
     sil::tensor::Tensor derivative_tensor(derivative_tensor_alloc);
 
-    sil::exterior::deriv<LaplacianDummyTag, CochainTag>(exec_space, derivative_tensor, tensor);
+    sil::exterior::deriv<LaplacianDummyIndex, CochainTag>(exec_space, derivative_tensor, tensor);
     Kokkos::fence();
 
     // Codifferential
     sil::exterior::codifferential<
             MetricIndex,
-            LaplacianDummyTag,
+            LaplacianDummyIndex,
             coboundary_index_t<
-                    LaplacianDummyTag,
+                    LaplacianDummyIndex,
                     CochainTag>>(exec_space, laplacian_tensor, derivative_tensor, inv_metric);
     Kokkos::fence();
 
