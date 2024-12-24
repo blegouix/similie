13375fa19a287d6ec2875ae24f9bb284fe5b5857
Baptiste Legouix <stilynx51@gmail.com>
2024-12-24 15:25:59 +0000

    wip

--- include/similie/exterior/laplacian.hpp
+++ include/similie/exterior/laplacian.hpp
@@ -26,7 +26,7 @@ template <
         misc::Specialization<tensor::Tensor> TensorType,
         misc::Specialization<tensor::Tensor> MetricType,
         class ExecSpace>
-TensorType codifferential_of_deriv(
+TensorType codifferential_of_coboundary(
         ExecSpace const& exec_space,
         TensorType out_tensor,
         TensorType tensor,
@@ -59,6 +59,49 @@ TensorType codifferential_of_deriv(
     return out_tensor;
 }
 
+template <
+        tensor::TensorIndex MetricIndex,
+        tensor::TensorNatIndex LaplacianDummyIndex,
+        tensor::TensorIndex CochainTag,
+        misc::Specialization<tensor::Tensor> TensorType,
+        misc::Specialization<tensor::Tensor> MetricType,
+        class ExecSpace>
+TensorType coboundary_of_codifferential(
+        ExecSpace const& exec_space,
+        TensorType out_tensor,
+        TensorType tensor,
+        MetricType inv_metric)
+{
+    // Codifferential
+    [[maybe_unused]] tensor::TensorAccessor<codifferential_index_t<LaplacianDummyIndex, CochainTag>>
+            codifferential_accessor;
+    ddc::cartesian_prod_t<
+            typename TensorType::non_indices_domain_t,
+            ddc::DiscreteDomain<codifferential_index_t<LaplacianDummyIndex, CochainTag>>>
+            codifferential_tensor_dom(
+                    tensor.non_indices_domain(),
+                    codifferential_accessor.mem_domain());
+    ddc::Chunk codifferential_tensor_alloc(
+            codifferential_tensor_dom,
+            ddc::KokkosAllocator<double, typename ExecSpace::memory_space>());
+    sil::tensor::Tensor codifferential_tensor(codifferential_tensor_alloc);
+
+    sil::exterior::codifferential<
+            MetricIndex,
+            LaplacianDummyIndex,
+            coboundary_index_t<
+                    LaplacianDummyIndex,
+                    CochainTag>>(exec_space, codifferential_tensor, tensor, inv_metric);
+    Kokkos::fence();
+
+    // Derivative
+    sil::exterior::
+            deriv<LaplacianDummyIndex, CochainTag>(exec_space, out_tensor, codifferential_tensor);
+    Kokkos::fence();
+
+    return out_tensor;
+}
+
 } // namespace detail
 
 template <
@@ -77,64 +120,48 @@ TensorType laplacian(
     static_assert(tensor::is_covariant_v<LaplacianDummyIndex>);
 
     if constexpr (CochainTag::rank() == 0) {
-        // Derivative
-        [[maybe_unused]] tensor::TensorAccessor<coboundary_index_t<LaplacianDummyIndex, CochainTag>>
-                derivative_accessor;
-        ddc::cartesian_prod_t<
-                typename TensorType::non_indices_domain_t,
-                ddc::DiscreteDomain<coboundary_index_t<LaplacianDummyIndex, CochainTag>>>
-                derivative_tensor_dom(
-                        tensor.non_indices_domain(),
-                        derivative_accessor.mem_domain());
-        ddc::Chunk derivative_tensor_alloc(
-                derivative_tensor_dom,
-                ddc::KokkosAllocator<double, typename ExecSpace::memory_space>());
-        sil::tensor::Tensor derivative_tensor(derivative_tensor_alloc);
-
-        sil::exterior::
-                deriv<LaplacianDummyIndex, CochainTag>(exec_space, derivative_tensor, tensor);
-        Kokkos::fence();
-
-        // Codifferential
-        sil::exterior::codifferential<
+        detail::codifferential_of_coboundary<
                 MetricIndex,
                 LaplacianDummyIndex,
                 coboundary_index_t<
                         LaplacianDummyIndex,
-                        CochainTag>>(exec_space, laplacian_tensor, derivative_tensor, inv_metric);
+                        CochainTag>>(exec_space, laplacian_tensor, tensor, inv_metric);
         Kokkos::fence();
 
-        return laplacian_tensor;
     } else if (CochainTag::rank() == LaplacianDummyIndex::size()) {
-        // Codifferential
-        [[maybe_unused]] tensor::TensorAccessor<
-                codifferential_index_t<LaplacianDummyIndex, CochainTag>> codifferential_accessor;
-        ddc::cartesian_prod_t<
-                typename TensorType::non_indices_domain_t,
-                ddc::DiscreteDomain<codifferential_index_t<LaplacianDummyIndex, CochainTag>>>
-                codifferential_tensor_dom(
-                        tensor.non_indices_domain(),
-                        codifferential_accessor.mem_domain());
-        ddc::Chunk codifferential_tensor_alloc(
-                codifferential_tensor_dom,
-                ddc::KokkosAllocator<double, typename ExecSpace::memory_space>());
-        sil::tensor::Tensor codifferential_tensor(codifferential_tensor_alloc);
-
-        sil::exterior::codifferential<
+        detail::coboundary_of_codifferential<
                 MetricIndex,
                 LaplacianDummyIndex,
                 coboundary_index_t<
                         LaplacianDummyIndex,
-                        CochainTag>>(exec_space, laplacian_tensor, derivative_tensor, inv_metric);
+                        CochainTag>>(exec_space, laplacian_tensor, tensor, inv_metric);
+        Kokkos::fence();
+    } else {
+        detail::codifferential_of_coboundary<
+                MetricIndex,
+                LaplacianDummyIndex,
+                coboundary_index_t<
+                        LaplacianDummyIndex,
+                        CochainTag>>(exec_space, laplacian_tensor, tensor, inv_metric);
+
+        auto tmp_alloc = ddc::create_mirror_view(laplacian_tensor);
+        tensor::Tensor tmp(tmp_alloc);
+        detail::coboundary_of_codifferential<
+                MetricIndex,
+                LaplacianDummyIndex,
+                coboundary_index_t<
+                        LaplacianDummyIndex,
+                        CochainTag>>(exec_space, tmp, tensor, inv_metric);
         Kokkos::fence();
 
-        // Derivative
-        sil::exterior::
-                deriv<LaplacianDummyIndex, CochainTag>(exec_space, codifferential_tensor, tensor);
-        Kokkos::fence();
-
-        return laplacian_tensor;
+        ddc::parallel_for_each(
+                exec_space,
+                laplacian_tensor.domain(),
+                KOKKOS_LAMBDA(typename TensorType::discrete_element_type elem) {
+                    laplacian_tensor(elem) += tmp(elem);
+                });
     }
+    return laplacian_tensor;
 }
 
 } // namespace exterior
