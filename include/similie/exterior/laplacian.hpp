098998e9579dfe3d5db1045c8fa1dc70674f749b
Baptiste Legouix <stilynx51@gmail.com>
2024-12-24 14:17:24 +0000

    wip

--- include/similie/exterior/laplacian.hpp
+++ include/similie/exterior/laplacian.hpp
@@ -17,6 +17,50 @@ namespace sil {
 
 namespace exterior {
 
+namespace detail {
+
+template <
+        tensor::TensorIndex MetricIndex,
+        tensor::TensorNatIndex LaplacianDummyIndex,
+        tensor::TensorIndex CochainTag,
+        misc::Specialization<tensor::Tensor> TensorType,
+        misc::Specialization<tensor::Tensor> MetricType,
+        class ExecSpace>
+TensorType codifferential_of_deriv(
+        ExecSpace const& exec_space,
+        TensorType out_tensor,
+        TensorType tensor,
+        MetricType inv_metric)
+{
+    // Derivative
+    [[maybe_unused]] tensor::TensorAccessor<coboundary_index_t<LaplacianDummyIndex, CochainTag>>
+            derivative_accessor;
+    ddc::cartesian_prod_t<
+            typename TensorType::non_indices_domain_t,
+            ddc::DiscreteDomain<coboundary_index_t<LaplacianDummyIndex, CochainTag>>>
+            derivative_tensor_dom(tensor.non_indices_domain(), derivative_accessor.mem_domain());
+    ddc::Chunk derivative_tensor_alloc(
+            derivative_tensor_dom,
+            ddc::KokkosAllocator<double, typename ExecSpace::memory_space>());
+    sil::tensor::Tensor derivative_tensor(derivative_tensor_alloc);
+
+    sil::exterior::deriv<LaplacianDummyIndex, CochainTag>(exec_space, derivative_tensor, tensor);
+    Kokkos::fence();
+
+    // Codifferential
+    sil::exterior::codifferential<
+            MetricIndex,
+            LaplacianDummyIndex,
+            coboundary_index_t<
+                    LaplacianDummyIndex,
+                    CochainTag>>(exec_space, out_tensor, derivative_tensor, inv_metric);
+    Kokkos::fence();
+
+    return out_tensor;
+}
+
+} // namespace detail
+
 template <
         tensor::TensorIndex MetricIndex,
         tensor::TensorNatIndex LaplacianDummyIndex,
