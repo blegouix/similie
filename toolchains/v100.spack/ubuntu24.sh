be4c2a889619f2c9848a5fc73167f111c70e564b
blegouix <stilynx51@gmail.com>
2025-01-01 21:12:51 +0100

    update toolchain

--- toolchains/v100.spack/ubuntu24.sh
+++ toolchains/v100.spack/ubuntu24.sh
@@ -4,6 +4,8 @@
 sudo apt install -y build-essential
 sudo apt install -y cmake
 sudo apt install -y bzip2 ca-certificates file gcc-13 g++-13 gfortran-13 git gzip lsb-release patch python3 tar unzip xz-utils zstd
+sudo apt install -y libssl-dev
+sudo apt install -y libomp-19-dev
 if ! nvidia-smi &> /dev/null; then
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
     sudo dpkg -i cuda-keyring_1.1-1_all.deb
