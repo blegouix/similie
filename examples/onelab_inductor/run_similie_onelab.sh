0df7cbcc800eb305685cad3e18025c899f6e9b44
blegouix <stilynx51@gmail.com>
2026-06-06 11:35:35 +0200

    wip

--- examples/onelab_inductor/run_similie_onelab.sh
+++ examples/onelab_inductor/run_similie_onelab.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2026 Baptiste Legouix
+# SPDX-License-Identifier: MIT
+
 set -euo pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
